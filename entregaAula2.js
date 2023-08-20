13d0f33db18b5f66cfecaf39f6f045286212b318
Lucas Costabile <111201828+LucasCostabile@users.noreply.github.com>
2023-08-20 17:30:08 -0300

    Add files via upload

---
+++ entregaAula2.js
@@ -0,0 +1,10 @@
+var idade = 28;  // foi criado uma variavel "idade" para comparar minha idade com a do usuario
+
+
+var idadeUsuario = prompt('digite sua idade');
+var diferencaIdade = idade - idadeUsuario;
+alert("Olha só, nós temos " + diferencaIdade + " anos de diferença");
+
+var palavraUsuario = prompt('digite uma palavra');
+var palavraUsuario2 = prompt('digite uma outra palavra para concatenar com a primeira');
+alert("Confira o resultado:  " + palavraUsuario + palavraUsuario2);
